ebe03502fc344ebca401802069aed75cf6b02702
guricerin <gurizumi@gmail.com>
2019-05-06 08:45:33 +0900

    add cpsco2019-s1

---
+++ atcoder/volunteer-contest/cpsco2019-session1/b.rs
@@ -0,0 +1,85 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input!(s: chars);
+
+    let mut cs = vec![0; 26];
+    for &c in &s {
+        cs[(c as u8 - b'a') as usize] += 1;
+    }
+    let mut map = HashSet::new();
+    for &c in cs.iter() {
+        if c != 0 {
+            map.insert(c);
+        }
+    }
+
+    println!("{}", if map.len() == 1 { "Yes" } else { "No" });
+}

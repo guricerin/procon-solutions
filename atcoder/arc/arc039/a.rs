987a2e2034cf2339904db6fe82bf5828dd5dee10
guricerin <gurizumi@gmail.com>
2019-05-07 06:22:18 +0000

    add arc dir

---
+++ atcoder/arc/arc039/a.rs
@@ -0,0 +1,88 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_imports)]
+use std::collections::{HashMap};
+
+fn main() {
+    input!(a:i64,b:i64);
+
+    let f = |x| [x/ 100, x%100/10, x%10];
+    let g = |a,b,c| a*100 + b*10+c;
+
+    let aa = f(a);
+    let bb = f(b);
+    let mut vs = vec![
+        g(9,aa[1],aa[2]) - b,
+        g(aa[0],9,aa[2]) -b,
+        g(aa[0],aa[1],9) - b,
+        a - g(1,bb[1],bb[2]),
+        a - g(bb[0],0,bb[2]),
+        a - g(bb[0],bb[1],0)
+    ];
+    vs.sort();
+    println!("{}", vs.last().unwrap());
+}

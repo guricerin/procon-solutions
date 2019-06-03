801b92c1b02b37932afd5bfa957859c822972e23
guricerin <gurizumi@gmail.com>
2019-06-03 23:26:52 +0900

    add abc106

---
+++ atcoder/abc/abc106/d.rs
@@ -0,0 +1,96 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::HashMap;
+
+fn main() {
+    input!(
+        n: usize,
+        m: usize,
+        q: usize,
+        lr: [(usize, usize); m],
+        pq: [(usize, usize); q]
+    );
+
+    let mut acc = vec![vec![0i64; n + 10]; n + 10];
+    for &(l, r) in &lr {
+        acc[l][r] += 1;
+    }
+
+    for i in 1..n + 1 {
+        for j in 1..n + 1 {
+            acc[i][j] += acc[i - 1][j] + acc[i][j - 1] - acc[i - 1][j - 1];
+        }
+    }
+
+    for &(p, q) in &pq {
+        let ans = acc[q][q] - acc[p - 1][q] - acc[q][p - 1] + acc[p - 1][p - 1];
+        println!("{}", ans);
+    }
+}

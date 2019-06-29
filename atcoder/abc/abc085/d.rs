e55f4ee26cde8267a688487545b36c5cfaa624c5
guricerin <gurizumi@gmail.com>
2019-06-29 11:46:22 +0900

    add abc085

---
+++ atcoder/abc/abc085/d.rs
@@ -0,0 +1,109 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::BTreeMap;
+
+fn main() {
+    input!(n: usize, h: i64, ab: [(i64, i64); n]);
+
+    let a_max = ab.iter().map(|&x| x.0).max().unwrap();
+    let mut bs = ab
+        .iter()
+        .map(|&x| x.1)
+        .filter(|&x| a_max <= x)
+        .collect::<Vec<i64>>();
+    bs.sort();
+    bs.reverse();
+
+    let mut h = h;
+    let mut cnt = 0;
+    for &b in bs.iter() {
+        h -= b;
+        cnt += 1;
+        if h <= 0 {
+            println!("{}", cnt);
+            return;
+        }
+    }
+
+    if h <= 0 {
+        println!("{}", cnt);
+        return;
+    }
+    let rem = (h + a_max - 1) / a_max;
+    cnt += rem;
+    println!("{}", cnt);
+}

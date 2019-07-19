fef45e5522a427f0362fa3ab0eefbd876c61ad8a
guricerin <gurizumi@gmail.com>
2019-07-19 11:41:25 +0900

    add yuki-216-c

---
+++ yukicoder/contest-216/c.rs
@@ -0,0 +1,150 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(dead_code)]
+fn chmin<T>(x: &mut T, y: T) -> bool
+where
+    T: PartialOrd + Copy,
+{
+    *x > y && {
+        *x = y;
+        true
+    }
+}
+
+#[allow(dead_code)]
+fn chmax<T>(x: &mut T, y: T) -> bool
+where
+    T: PartialOrd + Copy,
+{
+    *x < y && {
+        *x = y;
+        true
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+
+fn main() {
+    input! {
+        n:usize
+    }
+    let sieve = eratosthenes_sieve(n);
+    let mut primes = vec![];
+    for (i, &v) in sieve.iter().enumerate() {
+        if v {
+            primes.push(i);
+        }
+    }
+
+    // 愚直解
+    let mut cnt = 0;
+    let lim = primes.len();
+    for i in 0..lim {
+        let r2 = primes[i].pow(2) as i64;
+        for j in 0..lim {
+            let p = primes[j] as i64;
+            let b = r2 - p;
+            if b < 0 || n < b as usize {
+                continue;
+            }
+            if sieve[b as usize] {
+                cnt += 1;
+            }
+        }
+    }
+    println!("{}", cnt);
+}
+
+pub fn eratosthenes_sieve(n: usize) -> Vec<bool> {
+    let n = n + 1;
+    let mut res = vec![true; n];
+    res[0] = false;
+    res[1] = false;
+    for i in 2..n {
+        if !res[i] {
+            continue;
+        }
+        for j in 2..n {
+            if i * j >= n {
+                break;
+            }
+            // 素数の倍数は素数ではない
+            res[i * j] = false;
+        }
+    }
+    res
+}

17bd9e083001bdabf25f817056fe24840e204afd
guricerin <gurizumi@gmail.com>
2019-07-19 10:05:39 +0900

    add yuki-181

---
+++ yukicoder/contest-181/b.rs
@@ -0,0 +1,124 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(dead_code)]
+fn chmin<T>(x: &mut T, y: T) -> bool
+where
+    T: PartialOrd + Copy,
+{
+    *x > y && {
+        *x = y;
+        true
+    }
+}
+
+#[allow(dead_code)]
+fn chmax<T>(x: &mut T, y: T) -> bool
+where
+    T: PartialOrd + Copy,
+{
+    *x < y && {
+        *x = y;
+        true
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+
+fn main() {
+    input! {
+        n:i64
+    }
+
+    // aの候補を3と5に絞ったのは、完全になんとなく。
+    for &a in [3, 5].iter() {
+        if n - a <= 0 {
+            break;
+        }
+        if f(n, a) {
+            println!("{} {}", a, n - a);
+            return;
+        } else {
+            // println!("{}", n - a);
+        }
+    }
+    println!("-1");
+}
+
+fn f(x: i64, a: i64) -> bool {
+    let b = x - a;
+    !(b as u64).is_power_of_two()
+}

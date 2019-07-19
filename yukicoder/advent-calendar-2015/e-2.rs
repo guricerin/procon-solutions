9ef3a7d1982a97a5ed4d70de0714dfda47d3f195
guricerin <gurizumi@gmail.com>
2019-07-19 13:56:37 +0900

    yuki-ad-eの別解

---
+++ yukicoder/advent-calendar-2015/e-2.rs
@@ -0,0 +1,135 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(dead_code)]
+fn chmin<T>(x: &mut T, y: T) -> bool
+where
+    T: PartialOrd + Copy,
+{
+    *x > y && {
+        *x = y;
+        true
+    }
+}
+
+#[allow(dead_code)]
+fn chmax<T>(x: &mut T, y: T) -> bool
+where
+    T: PartialOrd + Copy,
+{
+    *x < y && {
+        *x = y;
+        true
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+
+fn main() {
+    input! {
+        n:i64
+    }
+
+    // 「2より大きい最小の約数を答えよ」と言い換えられる
+    let divs = divisors(n);
+    let mut ans = n;
+    for &d in divs.iter() {
+        if d <= 2 {
+            continue;
+        }
+        chmin(&mut ans, d as i64);
+    }
+
+    println!("{}", ans);
+}
+
+pub fn divisors(n: i64) -> Vec<usize> {
+    let mut res = vec![];
+    let n = n as usize;
+    for i in 1.. {
+        if i * i > n {
+            break;
+        }
+        if n % i == 0 {
+            res.push(i);
+            if i * i != n {
+                res.push(n / i);
+            }
+        }
+    }
+    res.sort();
+    res
+}

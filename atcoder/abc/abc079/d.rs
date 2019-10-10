eb41f1fe5738916fb263fc6d05c578c6144b1056
guricerin <gurizumi@gmail.com>
2019-10-10 14:33:13 +0900

    add abc079-d

---
+++ atcoder/abc/abc079/d.rs
@@ -0,0 +1,153 @@
+// Original: https://github.com/tanakh/competitive-rs
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+mod util {
+    #[allow(dead_code)]
+    pub fn chmin<T>(x: &mut T, y: T) -> bool
+    where
+        T: PartialOrd + Copy,
+    {
+        *x > y && {
+            *x = y;
+            true
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn chmax<T>(x: &mut T, y: T) -> bool
+    where
+        T: PartialOrd + Copy,
+    {
+        *x < y && {
+            *x = y;
+            true
+        }
+    }
+
+    /// 整数除算切り上げ
+    #[allow(dead_code)]
+    pub fn roundup(a: i64, b: i64) -> i64 {
+        (a + b - 1) / b
+    }
+
+    #[allow(dead_code)]
+    pub fn ctoi(c: char) -> i64 {
+        c as i64 - 48
+    }
+}
+
+#[allow(unused_imports)]
+use util::*;
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+
+fn main() {
+    input! {
+        h:usize,w:usize,
+        mut cs:[[i64;10];10],
+        graph:[[i64;w];h]
+    }
+
+    let inf: i64 = 1 << 50;
+    warshall_floyd(&mut cs, inf);
+
+    let mut cost = 0;
+    for y in 0..h {
+        for x in 0..w {
+            let n = graph[y][x];
+            if n == -1 || n == 1 {
+                continue;
+            }
+            cost += cs[n as usize][1];
+        }
+    }
+    println!("{}", cost);
+}
+
+pub fn warshall_floyd(matrix: &mut Vec<Vec<i64>>, inf: i64) {
+    let n = matrix.len();
+    assert_eq!(n, matrix[0].len());
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                if matrix[i][k] == inf || matrix[k][j] == inf {
+                    continue;
+                }
+                matrix[i][j] = std::cmp::min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+            }
+        }
+    }
+}

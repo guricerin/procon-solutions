449baaf00664be87a19a7e7ef74e80e57b68622d
guricerin <gurizumi@gmail.com>
2019-12-09 23:15:09 +0900

    add yuki-230

---
+++ yukicoder/contest-230/a.fs
@@ -0,0 +1,25 @@
+open System
+
+[<AutoOpen>]
+module Cin =
+    let read f = stdin.ReadLine() |> f
+    let reada f = stdin.ReadLine().Split() |> Array.map f
+
+    let readInts() =
+        read string
+        |> Seq.toArray
+        |> Array.map (fun x -> Convert.ToInt32(x.ToString()))
+
+module Util =
+    let strRev s =
+        s
+        |> Seq.rev
+        |> Seq.map string
+        |> String.concat ""
+
+[<EntryPoint>]
+let main _ =
+    let [|X;Y;Z|] = reada int
+    List.min [X+Z; Y+Z; (X+Y+Z)/2]
+    |> printfn "%d"
+    0 // return an integer exit code

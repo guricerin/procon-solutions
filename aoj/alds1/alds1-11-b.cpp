9f1a582633d690d8fc9a2d27c711f02ec437a061
guricerin <gurizumi@gmail.com>
2020-03-12 23:48:57 +0900

    [add] aoj

---
+++ aoj/alds1/alds1-11-b.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i32 = int32_t;
+using i64 = int64_t;
+using f32 = float;
+using f64 = double;
+using P   = pair<int, int>;
+
+template <class T>
+bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    } else {
+        return false;
+    }
+}
+template <class T>
+bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <class T>
+void dump_vec(const vector<T>& v) {
+    auto len = v.size();
+    rep(i, 0, len) {
+        cout << v[i] << (i == (int)len - 1 ? "\n" : " ");
+    }
+}
+
+struct FastIO {
+    FastIO() {
+        cin.tie(0);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(20);
+    }
+} FASTIO;
+
+//---------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+
+vector<int> ds;
+vector<int> fs;
+vector<vector<int>> G;
+
+int stamp = 0;
+
+void dfs(int u) {
+    if (ds[u] >= 0) return;
+    ds[u] = ++stamp;
+
+    for (auto& v : G[u]) {
+        dfs(v);
+    }
+    fs[u] = ++stamp;
+}
+
+signed main() {
+    int N;
+    cin >> N;
+    G.resize(N);
+    ds.assign(N, -1);
+    fs.assign(N, -1);
+    rep(i, 0, N) {
+        int id, k;
+        cin >> id >> k;
+        id--;
+        rep(j, 0, k) {
+            int u;
+            cin >> u;
+            u--;
+            G[id].push_back(u);
+        }
+        sort(all(G[id]));
+    }
+    rep(i, 0, N) {
+        dfs(i);
+    }
+    rep(i, 0, N) {
+        cout << i + 1 << " " << ds[i] << " " << fs[i] << "\n";
+    }
+}

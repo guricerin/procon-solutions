996549acee376776a8dd13d29744c0e4260359f7
guricerin <gurizumi@gmail.com>
2020-03-26 11:20:04 +0900

    add

---
+++ atcoder/others/panasonic2020/c.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i32 = int32_t;
+using i64 = int64_t;
+using f32 = float;
+using f64 = double;
+using P = pair<int, int>;
+
+template <class T>
+bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    } else {
+        return false;
+    }
+}
+template <class T>
+bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <class T>
+void dump_vec(const vector<T>& v) {
+    auto len = v.size();
+    rep(i, 0, len) {
+        cout << v[i] << (i == (int)len - 1 ? "\n" : " ");
+    }
+}
+
+struct FastIO {
+    FastIO() {
+        cin.tie(0);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(20);
+    }
+} FASTIO;
+
+//---------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+
+signed main() {
+    i64 a, b, c;
+    cin >> a >> b >> c;
+    auto left = 4 * a * b;
+    auto r = c - a - b;
+    auto right = r * r;
+    bool ok = 0 <= r && left < right;
+    cout << (ok ? "Yes" : "No") << "\n";
+}

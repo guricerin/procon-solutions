d6d9b6e32328307da80576c70d53d34978d3bcaa
guricerin <gurizumi@gmail.com>
2020-04-07 17:20:11 +0900

    [add]

---
+++ atcoder/unrated/judge-update-202004/b.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i32 = int32_t;
+using i64 = int64_t;
+using f32 = float;
+using f64 = double;
+using P = pair<int, int>;
+
+template <class T>
+bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    } else {
+        return false;
+    }
+}
+template <class T>
+bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <class T>
+void dump_vec(const vector<T>& v) {
+    auto len = v.size();
+    rep(i, 0, len) {
+        cout << v[i] << (i == (int)len - 1 ? "\n" : " ");
+    }
+}
+
+struct FastIO {
+    FastIO() {
+        cin.tie(0);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(20);
+    }
+} FASTIO;
+
+//---------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+
+signed main() {
+    int N;
+    cin >> N;
+    vector<int> red, blue;
+    rep(i, 0, N) {
+        int x;
+        cin >> x;
+        char c;
+        cin >> c;
+        if (c == 'R')
+            red.push_back(x);
+        else
+            blue.push_back(x);
+    }
+    sort(all(red));
+    sort(all(blue));
+    for (const auto& x : red) {
+        cout << x << "\n";
+    }
+    for (const auto& x : blue) {
+        cout << x << "\n";
+    }
+}
